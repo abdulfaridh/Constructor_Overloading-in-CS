726dbba7e768cee8031de7ab8354b7b14796fc37
farid <farid@LAPTOP-SPTVSQVM>
2024-08-07 22:14:38 +0530

    Add project files.

---
+++ Constructor_Overloading/Constructor.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace one
+{
+    public class Constructor
+    {
+        public Constructor()
+        {
+            Console.WriteLine("a empty constructor");
+        }
+        public Constructor(int i)
+        {
+            Console.WriteLine("your number is {0}",i);
+        }
+        public Constructor(int i,string s)
+        {
+            Console.WriteLine("your number is {0} and your string is  {1}",i,s);
+        }
+    }
+}
---
+++ Constructor_Overloading/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace one
+{
+    public class MainClass
+    {
+        static void Main(String[] args)
+        {
+            var c = new Constructor(1,"abdul");
+        }
+
+    }
+}
